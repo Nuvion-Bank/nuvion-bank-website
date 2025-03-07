eb7bd310227d7eb070740f18b836a4214aed86f2
samuel13-ai <gichukisamuel91@gmail.com>
2025-03-07 11:55:50 +0300

    Added Footer

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<?php
+// footer.php
+?>
+<footer class="footer">
+    <p>&copy; <?php echo date("Y"); ?>Nuvion Bank. All rights reserved.</p>
+
+    <ul class="footer-links">
+        <li><a href="privacy-policy.php">Privacy Policy</a></li>
+        <li><a href="terms-of-service.php">Terms of Service</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+    </ul>
+
+    <p class="contact-info">
+        📧 Email: <a href="mailto:support@mywebsite.com">support@NuvionBANK.com</a> | 
+        📞 Phone: <a href="tel:+254115348341"></a>
+    </p>
+</footer>
