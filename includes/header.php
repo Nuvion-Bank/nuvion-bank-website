1a8c81d0576512590e0ea68c53d294c9dcac6bea
okenomogoii <150903083+okenomogoii@users.noreply.github.com>
2025-03-07 11:30:24 +0300

    Header files

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banking</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="logo">
+                <a href="#"> Nuvion Bank</a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Rewards</a></li>
+                    <li><a href="#">Stock</a></li>
+                    <li><a href="#">Credit</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#" class="sign-in">Sign In</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
